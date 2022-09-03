cb7c263b1f878bda56dbd9a7837df2c04b22bb48
GeniusHawlah <geniusdecode@gmail.com>
2022-09-03 14:17:31 +0100

    Added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,44 @@
-import { Fragment, useEffect } from 'react';
-import { AllContextProvider } from '../a-store/context-store/AllContext';
-import Footer from '../components/Footer';
-import MegaMenu from '../components/MegaMenu';
-import Navbar from '../components/Navbar';
-import '../styles/globals.css'
+import React, { Fragment, useEffect } from "react";
+import Script from "next/script";
+import { AllContextProvider } from "../a-store/context-store/AllContext";
+import Footer from "../components/Footer";
+import MegaMenu from "../components/MegaMenu";
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-
   useEffect(() => {
     const use = async () => {
-      (await import('tw-elements')).default;
-        };
-        use();
-      }, []); 
+      (await import("tw-elements")).default;
+    };
+    use();
+  }, []);
 
+  return (
+    <Fragment>
+      <AllContextProvider>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          strategy="afterInteractive"
+        />
 
-  return <Fragment >
-    <AllContextProvider>
-      
-      <Navbar />
-     <Component {...pageProps} />
-    </AllContextProvider>
-  
-     <Footer/>
-  </Fragment>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);} 
+        gtag('js', new Date());
+
+        gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+        `}
+        </Script>
+
+        <Navbar />
+        <Component {...pageProps} />
+      </AllContextProvider>
+
+      <Footer />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
