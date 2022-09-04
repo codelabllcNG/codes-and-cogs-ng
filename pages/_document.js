671360fe817f3d27e097ea9e0beb9776c6395547
GeniusHawlah <geniusdecode@gmail.com>
2022-09-04 16:17:45 +0100

    Fixed G is not defined error

--- pages/_document.js
+++ pages/_document.js
@@ -8,18 +8,7 @@ class MyDocument extends Document {
       
   <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/22619367.js"></script>
 
-  {/* <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js?pre=1"></script>
-<script dangerouslySetInnerHTML={{
-          __html: `
-          hbspt.forms.create({
-            region: "na1",
-            portalId: "22606943",
-            formId: "28241b1b-34b7-449d-9b15-85a2367d0a76",
-            target: "#id"
-          });
-`}} >
-
-</script> */}
+ 
 
         <body>
           <Main />
