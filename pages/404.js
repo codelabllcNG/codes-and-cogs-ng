af6b25f0e71feb1dcf4cc9b894f3771b558e3ee9
GeniusHawlah <geniusdecode@gmail.com>
2022-09-13 05:04:15 +0100

    Added custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function A404Page() {
+  return (
+      <div className='w-full h-full flex justify-center items-center'>
+          <p className='text-lg text-pry-color'>Error 404 - The page you are looking for does not exist.</p>
+    </div>
+  )
+}
+
+export default A404Page
