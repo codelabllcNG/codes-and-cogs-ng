6096a11e9f0ec24b4555ed4dadaaca53298c2eaa
GeniusHawlah <geniusdecode@gmail.com>
2022-09-19 13:51:54 +0100

    Header banner changed to webp

--- components/HeaderBanner.js
+++ components/HeaderBanner.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function HeaderBanner(props) {
   return (
-    <div dangerouslySetInnerHTML={{ __html: props.title.toUpperCase() }} className="bg-[url('/images/header-banner.png')] bg-center bg-cover bg-no-repeat  text-xs 320:text-base 350:text-2xl sm:text-2xl md:text-4xl font-larken text-white px-5 md:px-10 lg:px-24 -mx-5 md:-mx-10  lg:-mx-16 py-5 md:py-10 lg:py-12 ">
+    <div dangerouslySetInnerHTML={{ __html: props.title.toUpperCase() }} className="bg-[url('/images/header-banner.webp')] bg-center bg-cover bg-no-repeat  text-xs 320:text-base 350:text-2xl sm:text-2xl md:text-4xl font-larken text-white px-5 md:px-10 lg:px-24 -mx-5 md:-mx-10  lg:-mx-16 py-5 md:py-10 lg:py-12 ">
      {}
     </div>
   )
