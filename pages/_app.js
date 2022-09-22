a722ee43f675cde22b82bb5f112253c96cbc55ae
GeniusHawlah <geniusdecode@gmail.com>
2022-09-23 00:12:49 +0100

    Testing facebook pixel

--- pages/_app.js
+++ pages/_app.js
@@ -33,6 +33,31 @@ export default function MyApp({ Component, pageProps }) {
           strategy="afterInteractive"
         />
 
+
+<Script strategy="afterInteractive" id="fb-pixel"
+      
+          dangerouslySetInnerHTML={{
+            __html:
+              `
+              !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '453085319950484');
+          fbq('track', 'PageView');
+              `,
+          }}
+
+/>
+<noscript><img height="1" width="1" 
+src="https://www.facebook.com/tr?id=453085319950484&ev=PageView&noscript=1"
+/></noscript>
+
+
         <Script id="google-analytics" strategy="afterInteractive">
           {`
         window.dataLayer = window.dataLayer || [];
@@ -43,7 +68,7 @@ export default function MyApp({ Component, pageProps }) {
         `}
         </Script>
 
-
+ 
 
         <Navbar />
         <Component {...pageProps} />
