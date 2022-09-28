74f3bc8de6c77aa98c5832a46af22d241a410100
GeniusHawlah <geniusdecode@gmail.com>
2022-09-28 19:37:50 +0100

    Moved fb pixel code into Head

--- pages/_app.js
+++ pages/_app.js
@@ -53,6 +53,24 @@ export default function MyApp({ Component, pageProps }) {
           {/* DEV VERSION  */}
           <meta name="facebook-domain-verification" content="cdtzab6dg5iu52azrz30ek3p55i0bj" />
 
+          <Script
+          strategy="afterInteractive"
+          id="fb-pixel"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '648475550125111');
+          fbq('track', 'PageView');
+              `,
+          }}
+        />
 
         </Head>
         <Script
