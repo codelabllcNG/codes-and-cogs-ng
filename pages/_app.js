0bd16ece5c764dfbf1b75a5f102652a224130d41
GeniusHawlah <geniusdecode@gmail.com>
2022-09-28 18:12:32 +0100

    GTM testing

--- pages/_app.js
+++ pages/_app.js
@@ -66,7 +66,7 @@ export default function MyApp({ Component, pageProps }) {
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '671379057370383');
+          fbq('init', '648475550125111');
           fbq('track', 'PageView');
               `,
           }}
--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,7 @@ class MyDocument extends Document {
               height="1"
               width="1"
               style={{ display: "none" }}
-              src="https://www.facebook.com/tr?id=671379057370383&ev=PageView&noscript=1"
+              src="https://www.facebook.com/tr?id=648475550125111&ev=PageView&noscript=1"
             />
           </noscript>
          
