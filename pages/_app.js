ccae15dd4f78a119f494cae0fa76bacd8e92b3f3
GeniusHawlah <geniusdecode@gmail.com>
2022-09-28 13:48:43 +0100

    Added GTA id

--- pages/_app.js
+++ pages/_app.js
@@ -75,7 +75,7 @@ export default function MyApp({ Component, pageProps }) {
         
 <Script
           // strategy="afterInteractive"
-          // id="fb-pixel"
+          id="gta"
           dangerouslySetInnerHTML={{
             __html: `
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
