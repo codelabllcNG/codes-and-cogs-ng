137976a850515ad42e6251fbb8dcf7c759c38f23
GeniusHawlah <geniusdecode@gmail.com>
2022-09-29 15:07:31 +0100

    Retains only Live domain name verification

--- pages/_app.js
+++ pages/_app.js
@@ -48,13 +48,11 @@ export default function MyApp({ Component, pageProps }) {
           />
 
           {/* DEV VERSION  */}
-          <meta
+          {/* <meta
             name="facebook-domain-verification"
             content="cdtzab6dg5iu52azrz30ek3p55i0bj"
-          />
+          /> */}
 
-          {/* DEV TESTING  */}
-          <meta name="facebook-domain-verification" content="hpb1j6bjaeu3tfepvq8o7adshp69y3" />
 
         </Head>
         <Script
