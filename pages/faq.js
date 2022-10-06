4d26755b5e01a1cb5370c304e927c504bdfbf47b
GeniusHawlah <geniusdecode@gmail.com>
2022-10-06 15:36:18 +0100

    Key error

--- pages/faq.js
+++ pages/faq.js
@@ -16,7 +16,7 @@ const {faqs} = props;
 
           <div className='  grid grid-cols-1 md:grid-cols-2 gap-5' >
               {faqs.map(faq => <div className=''>
-                  <div className="text-pry-color text-2xl mt-5 font-semibold " dangerouslySetInnerHTML={{ __html: faq.title}}>
+                  <div key={faq.title} className="text-pry-color text-2xl mt-5 font-semibold " dangerouslySetInnerHTML={{ __html: faq.title}}>
                       {}
                   </div>
 
