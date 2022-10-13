ca7df37b2397c91415121250492f18761cab70f0
GeniusHawlah <geniusdecode@gmail.com>
2022-10-13 12:47:35 +0100

    staticPageGenerationTimeout increased to 1000

--- next.config.js
+++ next.config.js
@@ -21,9 +21,11 @@ const nextConfig = {
     domains: ['orgsapi.com'],
   },
 
-  httpAgentOptions: {
-    keepAlive: false,
-  },
+  // httpAgentOptions: {
+  //   keepAlive: false,
+  // },
+
+  staticPageGenerationTimeout: "1000"
 
 }
 
