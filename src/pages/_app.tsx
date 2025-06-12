83e437a897b1d217ee5ad234d793595f56338380
vicorandy <vicorandyrichard26@gmail.com>
2025-06-12 10:08:38 -0400

    chore : added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Head from "next/head";
 import '../styles/globals.css';
+import Script from "next/script";
 
 
 // Load fonts
@@ -57,6 +58,30 @@ const theme = extendTheme({
 export default function MyApp({ Component, pageProps }:AppProps) {
   const queryClient = new QueryClient();
   return (
+    <>
+          <Script
+          strategy="afterInteractive"
+          id="gtag-base"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5SDVXFH');
+              `,
+          }}
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);} 
+        gtag('js', new Date());
+
+        gtag('config', "${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}");
+        `}
+        </Script>
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
       <Head>
@@ -82,6 +107,6 @@ export default function MyApp({ Component, pageProps }:AppProps) {
           />
       </ChakraProvider>
     </QueryClientProvider>
-
+    </>
   );
 }
